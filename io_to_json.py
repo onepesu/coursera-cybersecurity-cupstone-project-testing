6f5825927df9c9162d35f5fdca62da12e2d5e77a
Stephanos Papanikolopoulos <spapanik21@gmail.com>
2015-05-14 08:52:25 +0100

    io

--- io_to_json.py
+++ io_to_json.py
@@ -1,10 +1,29 @@
-import json
+from __future__ import print_function
+
+start = '    '
 
 
 def translate(input_, output):
-    out = {'test': []}
-    for input_string, output_dict in zip(input_, output):
-        output_dict.update({'input': input_string})
-        out['test'].append(output_dict)
+    print('{')
+    print(start + '"tests": [')
+    for n, io in enumerate(zip(input_, output)):
+        input_string, output_dict = io
+        if n != 0:
+            print(',')
+        else:
+            print('')
+        print(2*start + '{')
+        print(3*start + '"input": "' + input_string, end='"')
+        if output_dict.get('exit'):
+            print(',')
+            print(3*start + '"exit": "' + str(output_dict['exit']), end='"')
+        if output_dict.get('output'):
+            print(',')
+            print(3*start + '"output": "' + repr(output_dict['output'])[1:-1],
+                  end='"')
+        print('')
+        print(2*start + '}', end='')
 
-    return json.dumps(out)
+    print('')
+    print(start + ']')
+    print('}')
