9017d4051ed303e7c839a851de175248f72b427b
Stephanos Papanikolopoulos <spapanik21@gmail.com>
2015-05-20 09:17:33 +0100

    write to json directly

--- io_to_json.py
+++ io_to_json.py
@@ -1,29 +1,40 @@
 from __future__ import print_function
 
+import sys
+
 start = '    '
 
 
-def translate(input_, output):
-    print('{')
-    print(start + '"tests": [', end='')
+def _translate(input_, output, open_file=sys.stdout):
+    print('{', file=open_file)
+    print(start + '"tests": [', end='', file=open_file)
     for n, io in enumerate(zip(input_, output)):
         input_string, output_dict = io
         if n != 0:
-            print(',')
+            print(',', file=open_file)
         else:
-            print('')
-        print(2*start + '{')
-        print(3*start + '"input": "' + input_string, end='"')
+            print('', file=open_file)
+        print(2*start + '{', file=open_file)
+        print(3*start + '"input": "' + input_string, end='"', file=open_file)
         if output_dict.get('output'):
-            print(',')
+            print(',', file=open_file)
             print(3*start + '"output": "' + repr(output_dict['output'])[1:-1],
-                  end='"')
+                  end='"', file=open_file)
         if output_dict.get('exit'):
-            print(',')
-            print(3*start + '"exit": ' + str(output_dict['exit']), end='')
-        print('')
-        print(2*start + '}', end='')
+            print(',', file=open_file)
+            print(3*start + '"exit": ' + str(output_dict['exit']), end='',
+                  file=open_file)
+        print('', file=open_file)
+        print(2*start + '}', end='', file=open_file)
 
-    print('')
-    print(start + ']')
-    print('}')
+    print('', file=open_file)
+    print(start + ']', file=open_file)
+    print('}', file=open_file)
+
+
+def translate(input_, output, filename=None):
+    if filename is None:
+        _translate(input_, output)
+    else:
+        with open(filename, 'w') as open_file:
+            _translate(input_, output, open_file)
