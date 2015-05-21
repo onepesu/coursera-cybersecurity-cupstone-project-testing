a38ed637e9114ce72db6fde0bc8db23a8791b324
Stephanos Papanikolopoulos <spapanik21@gmail.com>
2015-05-21 21:01:00 +0100

    improved test logging

--- check_test.py
+++ check_test.py
@@ -1,74 +1,78 @@
 #!/usr/bin/env python
+from __future__ import print_function
+
 import os
 import argparse
 import subprocess
 import base64
 import json
 
-def get_size(start_path = '.'):
-  total_size = 0
-  for dirpath, dirnames, filenames in os.walk(start_path):
-    for f in filenames:
-      fp = os.path.join(dirpath, f)
-      total_size += os.path.getsize(fp)
-  return total_size
 
-def main(testin, prefix, keeplog):
-  t = json.load(file(testin, 'r'))
-  #write out the batch file, if present
-  if t.has_key('batch'):
-    buf = t['batch']
-    buf = base64.b64decode(buf)
-    bufout = file('batch', 'w')
-    bufout.write(buf)
-    bufout.close()
-  #run all of the commands and test their outputs
-  cmds = t['tests']
-  passed = True
-  for i in cmds:
-    inpt = i['input']
-    args = inpt.split(" ")
-    cmd = args[0]
-    cmd = prefix + cmd
-    args[0] = cmd
-    print "running command %s" % inpt
-    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out,err = p.communicate()
-    ret = p.returncode
-    #check and see if out,err,ret match what is in this test
-    expectedout = ""
-    expectederr = ""
-    expectedret = 0
-    if i.has_key('output'):
-      expectedout = i['output']
-    if i.has_key('error'):
-      expectederr = i['error']
-    if i.has_key('exit'):
-      expectedret = i['exit']
-    out = out.replace("\n", "").replace("\r", "").replace(" ", "")
-    err = err.replace("\n", "").replace("\r", "").replace(" ", "")
-    expectedout = expectedout.replace("\n", "").replace("\r", "").replace(" ", "")
-    expectederr = expectederr.replace("\n", "").replace("\r", "").replace(" ", "")
-    if out != expectedout:
-      print "got %s expected %s" % (out, expectedout)
-      passed = False
-    if err != expectederr:
-      print "got %s expected %s" % (err, expectederr)
-      passed = False
-    if ret != expectedret:
-      print "got %d expected %d" % (ret, expectedret)
-      passed = False
-  if passed == True:
-    print "Test passed"
+def get_size(start_path='.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
 
-if __name__ =='__main__':
-  parser = argparse.ArgumentParser(description='Test executor')
-  parser.add_argument('--prefix', dest='prefix', type=str, default=".",
-                    help='program prefix')
-  parser.add_argument('--test', dest='test', type=str, default="test.json", required=True,
-                    help='test input')
-  parser.add_argument('--keep-logfile', dest='keeplog', type=bool, default=False,
-                    help='do not auto-erase the log file output')
 
-  args = parser.parse_args()
-  main(args.test, args.prefix, args.keeplog)
+def main(testin, prefix):
+    t = json.load(file(testin, 'r'))
+    # write out the batch file, if present
+    if 'batch' in t:
+        buf = t['batch']
+        buf = base64.b64decode(buf)
+        bufout = file('batch', 'w')
+        bufout.write(buf)
+        bufout.close()
+    # run all of the commands and test their outputs
+    cmds = t['tests']
+    passed = True
+    for i in cmds:
+        inpt = i['input']
+        args = inpt.split(" ")
+        cmd = prefix + args[0]
+        args[0] = cmd
+        print("running command {}".format(inpt))
+        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        ret = p.returncode
+        # check and see if out,err,ret match what is in this test
+        expectedout = ""
+        expectederr = ""
+        expectedret = 0
+        if 'output' in i:
+            expectedout = i['output']
+        if 'error' in i:
+            expectederr = i['error']
+        if 'exit' in i:
+            expectedret = i['exit']
+        truncated_out = out.replace("\n", "").replace("\r", "").replace(" ", "")
+        truncated_error = err.replace("\n", "").replace("\r", "").replace(" ", "")
+        truncated_expected_out = expectedout.replace("\n", "").replace("\r", "").replace(" ", "")
+        truncated_expected_error = expectederr.replace("\n", "").replace("\r", "").replace(" ", "")
+        if truncated_out != truncated_expected_out:
+            print("got:\n{}expected:\n{}".format(out, expectedout), end='')
+            passed = False
+        if truncated_error != truncated_expected_error:
+            print("got:\n{}expected:\n{}".format(err, expectederr), end='')
+            passed = False
+        if ret != expectedret:
+            print("got:\n{}expected:\n{}".format(ret, expectedret), end='')
+            passed = False
+    if passed is True:
+        print("Test passed")
+    else:
+        print("Test failed")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Test executor')
+    parser.add_argument('--prefix', dest='prefix', type=str, default=".",
+                        help='program prefix')
+    parser.add_argument('--test', dest='test', type=str, default="test.json", required=True,
+                        help='test input')
+
+    args = parser.parse_args()
+    main(args.test, args.prefix)
--- run_tests.py
+++ run_tests.py
@@ -8,15 +8,18 @@ import argparse
 
 from settings import BUILD_PATH
 
+OK = '\033[92m[OK]\033[0m'
+FAIL = '\033[91m[FAIL]\033[0m'
 json_re = re.compile('.*\.json$')
 
 current_dir = os.getcwd()
 test_dir = os.path.join(current_dir, 'tests')
-test_list = [os.path.join(path, filename) for path, _, files
-             in os.walk(test_dir) for filename in files
-             if re.search(json_re, filename)]
+test_list = [
+    os.path.join(path, filename) for path, _, files in os.walk(test_dir)
+    for filename in files if re.search(json_re, filename)
+]
 
-test_prefix = ("python check_test.py --prefix {}/ --test ".format(BUILD_PATH))
+test_prefix = 'python check_test.py --prefix {}/ --test '.format(BUILD_PATH)
 
 to_keep = ('run_tests.py', 'check_test.py', 'settings.template',
            'settings.py', '.gitignore', 'io_to_json.py')
@@ -36,6 +39,7 @@ args = parser.parse_args()
 
 clean_folder()
 os.system('touch test_log')
+os.system('touch clean_test_log')
 
 if not args.test:
     custom_list = test_list
@@ -47,22 +51,25 @@ else:
         )
     ]
 
+n = max([len(test_name) for test_name in custom_list])
+
 for test_name in custom_list:
     clean_folder(log_files)
-    print(re.sub('^' + test_dir + os.sep, '', test_name), end='\t')
-    os.system('echo {} >> test_log'.format(test_name))
+    short_test_name = re.sub('^' + test_dir + os.sep, '', test_name)
+    print(short_test_name, end=' '*(n-len(test_name)) + '\t')
+    with open('test_log', 'a') as test_log:
+        with open('clean_test_log', 'a') as clean_test_log:
+            test_log.write('\n' + short_test_name + '\n')
+            clean_test_log.write('\n' + short_test_name + '\n')
     os.system(test_prefix + test_name + ' >> test_log')
     with open('test_log', 'r') as test_log:
         test_result = test_log.readlines()[-1]
+    with open('clean_test_log', 'a') as clean_test_log:
         if test_result == 'Test passed\n':
-            print('\033[92m[OK]\033[0m')
+            print(OK)
+            clean_test_log.write('test passed\n')
         else:
-            print('\033[91m[FAIL]\033[0m')
-
-with open('test_log', 'r') as source:
-    with open('clean_test_log', 'w') as target:
-        for line in source.readlines():
-            if 'running command' not in line and line != 'start_testing\n':
-                target.write(line)
+            print(FAIL)
+            clean_test_log.write('test failed\n')
 
 clean_folder(log_files)
